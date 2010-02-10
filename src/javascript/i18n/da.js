d6c1706c4dbf042eae10bde8c1fb8ebe604f4800
olleolleolle <olle.jonsson@gmail.com>
2010-02-10 22:46:27 +0800

    da lang pack and a fix in the sv lang pack

---
+++ src/javascript/i18n/da.js
@@ -0,0 +1,13 @@
+// .po file like language pack
+plupload.addI18n({
+	'Select files' : 'Vælg filer',
+	'Add files to the upload queue and click the start button.' : 'Tilføj filer til køen, og tryk på start.',
+	'Filename' : 'Filnavn',
+	'Status' : 'Status',
+	'Size' : 'Størrelse',
+	'Add files to queue' : 'Tilføj filer til køen',
+	'Add files.' : 'Tilføj filer.',
+	'Stop current upload' : 'Stop upload',
+	'Start uploading queue' : 'Start upload',
+	'Drag files here.' : 'Træk filer her.'
+});
--- src/javascript/i18n/sv.js
+++ src/javascript/i18n/sv.js
@@ -1,7 +1,7 @@
 ﻿// .po file like language pack
 plupload.addI18n({
 	'Select files' : 'Välj filer',
-	'Add files to the upload queue and click the start button.' : 'Lägg till filter till kön och tryck på start.',
+	'Add files to the upload queue and click the start button.' : 'Lägg till filer till kön och tryck på start.',
 	'Filename' : 'Filnamn',
 	'Status' : 'Status',
 	'Size' : 'Storlek',
