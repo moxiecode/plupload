86317fef3ddf145bba5ab3120e7ef7c7e9716e23
Jakob A. Dam <jakob.a.dam@gmail.com>
2010-09-22 12:08:40 +0200

    util for parsing query string and getting a httpclient.

---
+++ src/java/plupload/src/plupload/HttpUtil.java
@@ -0,0 +1,41 @@
+package plupload;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+
+
+public class HttpUtil {
+
+	private static HttpClient httpclient;
+	
+	public static Map<String, String> parse_qs(String qs){
+		// i.d, not handling the general case where a list is possible
+		Map<String, String> map = new HashMap<String, String>();  
+		for (String param : qs.split("&")){  
+			String[] split = param.split("=");
+			String name = split[0];  
+			String value = split[1];  
+			map.put(name, value);  
+		}  
+		return map;
+	}
+	
+	public static HttpClient getHttpClient(){
+		if(httpclient != null){
+			return httpclient;
+		}
+		HttpParams params = new BasicHttpParams();
+		// Reuse connections. Very important for performance! 
+		params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION,
+				HttpVersion.HTTP_1_1);
+		httpclient = new DefaultHttpClient(params);
+		return httpclient;
+	}
+}
