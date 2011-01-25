999092f384fd7cf46d77a184376c6f1470e3b562
jayarjo <jayarjo@gmail.com>
2011-01-25 19:50:23 +0400

    Added Dutch language file. Closes #198.

---
+++ src/javascript/i18n/nl.js
@@ -0,0 +1,12 @@
+// Dutch
+plupload.addI18n({
+	'Select files' : 'Selecteer bestand(en):',
+	'Add files to the upload queue and click the start button.' : 'Voeg bestanden toe aan de wachtrij en druk op \'Start\'.',
+	'Filename' : 'Bestandsnaam',
+	'Status' : 'Status',
+	'Size' : 'Grootte',
+	'Add files' : 'Voeg bestanden toe',
+	'Stop current upload' : 'Stop upload',
+	'Start uploading queue' : 'Start upload',
+	'Drag files here.' : 'Sleep bestanden hierheen.'
+});
