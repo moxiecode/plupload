557eda5271898c209c838159b048bde16aeec31c
jayarjo <jayarjo@gmail.com>
2011-02-07 17:58:50 +0400

    Removed 'Content-type: text/plain; charset=UTF-8' header from upload.php, not needed and is known to cause blank window in HTML4 runtime.

--- examples/upload.php
+++ examples/upload.php
@@ -10,7 +10,6 @@
  */
 
 	// HTTP headers for no cache etc
-	header('Content-type: text/plain; charset=UTF-8');
 	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 	header("Cache-Control: no-store, no-cache, must-revalidate");
