9b5c78d4e4f4296a743298938e4c51f6ef64584f
jayarjo <jayarjo@gmail.com>
2011-02-13 20:28:51 +0400

    Added German, Russian and Spanish translations.

---
+++ src/javascript/i18n/de.js
@@ -0,0 +1,25 @@
+// German
+plupload.addI18n({
+	'Select files' : 'W&auml;hlen Sie die Dateien:',
+	'Add files to the upload queue and click the start button.' : 'Dateien hinzuf&uuml;gen und danach auf \'Starten des Uploads\' klicken und die Datei hochzuladen.',
+	'Filename' : 'Dateiname',
+	'Status' : 'Status',
+	'Size' : 'Gr&ouml;&szlig;e',
+	'Add files' : 'Hinzuf&uuml;gen von Dateien',
+	'Stop current upload' : 'Stop aktuellen Upload',
+	'Start uploading queue' : 'Starte Upload',
+	'Uploaded %d/%d files': '%d/%d Dateien sind Hochgeladen',
+	'N/A' : 'Nicht verf&uuml;gbar',
+	'Drag files here.' : 'Ziehen Sie die Dateien hier hin',
+	'File extension error.': 'Dateiendungs Fehler.',
+	'File size error.': 'Dateigr&ouml;ßen Fehler.',
+	'Init error.': 'Initialisierungs Fehler.',
+	'HTTP Error.': 'HTTP Fehler.',
+	'Security error.': 'Sicherheits Fehler.',
+	'Generic error.': 'Generic Fehler.',
+	'IO error.': 'Ein/Ausgabe Fehler.',
+	'Stop Upload': 'Stoppen des Uploads.',
+	'Add Files': 'Dateien hinzuf&uuml;gen',
+	'Start Upload': 'Starten des Uploads.',
+	'%d files queued': '%d Dateien in der Warteschlange.'
+});
