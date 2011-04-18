23c67ea2ffe5f1b5fbeabb34aa3836577ca25094
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2011-04-18 13:28:39 +0200

    host changed localhost -> 127.0.0.1. for some reason the java loader access the applet at 127.0.0.1 and that was not resolved

--- dev_appserver.py
+++ dev_appserver.py
@@ -9,4 +9,11 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.dirname(__file__))
 from werkzeug import run_simple
 from upload import app
 
-run_simple('localhost', 8080, app, use_debugger=False,use_reloader=True,threaded=False, processes=1)
+ROOT_PATH = os.environ['ROOT_PATH']
+STATIC_FILES = {
+    '/js_dev':  os.path.join(ROOT_PATH, 'src/javascript'),
+    '/js':  os.path.join(ROOT_PATH, 'js'),
+    '/': os.path.join(ROOT_PATH, 'examples')
+    }
+
+run_simple('127.0.0.1', 8080, app, use_debugger=True, use_reloader=True, threaded=False, processes=1, static_files=STATIC_FILES)
