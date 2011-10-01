e2eb226d107a8fba5d529871fcefdc153f08eec0
Spocke <spocke@moxiecode.com>
2011-10-01 12:35:57 +0200

    Fixed potential security issue if users doesn't properly remove the examples directory from a live site.

--- examples/dump.php
+++ examples/dump.php
@@ -26,7 +26,7 @@
 	</tr>
 	<?php $count = 0; foreach ($_POST as $name => $value) { ?>
 	<tr class="<?php echo $count % 2 == 0 ? 'alt' : ''; ?>">
-		<td><?php echo $name ?></td>
+		<td><?php echo htmlentities(stripslashes($name)) ?></td>
 		<td><?php echo nl2br(htmlentities(stripslashes($value))) ?></td>
 	</tr>
 	<?php } ?>
