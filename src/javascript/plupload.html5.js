3ba30df1de1e472ae02a239f9f89872501a8817f
Steve Mayhew <mayhew.steve@gmail.com>
2012-08-20 08:32:23 -0700

    Fix for HTML5 input z-index (issue #605)
    To work properly in qTip's and jQuery modal dialogs in WebKit browsers
    the input container must be 1 z-index below the button but not below
    any other prior elements (like another qTip or a modal dialog's model
    glasspane (the ui-widget-overlay).
    
    This issue (#605) was caused because the existing code does not
    consider the "current stacking context" when attempting to determine
    the z-index of the browse button.
    
    Here is a jsFiddle
    (http://jsfiddle.net/stevemayhew/VQ5NP/4/embedded/result/) that fixes
    the bug. With this verison of plupload and the snipit from the HTML5
    change.  And lastly, here is the jsFiddle that demonstrates the bug:
    http://jsfiddle.net/stevemayhew/PhCsp/embedded/result/

--- src/javascript/plupload.html5.js
+++ src/javascript/plupload.html5.js
@@ -197,8 +197,8 @@
 				chunks: sliceSupport,
 				// Safari on Windows has problems when selecting multiple files
 				multi_selection: !(plupload.ua.safari && plupload.ua.windows),
-				// Gecko 2+ can trigger file dialog programatically
-				triggerDialog: plupload.ua.gecko && window.FormData
+				// WebKit and Gecko 2+ can trigger file dialog progrmmatically
+				triggerDialog: (plupload.ua.gecko && window.FormData || plupload.ua.webkit)
 			};
 		},
 
@@ -465,10 +465,7 @@
 							});
 						}
 						
-						zIndex = parseInt(plupload.getStyle(browseButton, 'zIndex'), 10);
-						if (isNaN(zIndex)) {
-							zIndex = 0;
-						}						
+						zIndex = plupload.getZindex(browseButton);
 							
 						plupload.extend(browseButton.style, {
 							zIndex : zIndex
