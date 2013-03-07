96742a86272fe18ae4cb1993d335227e9f2dcf16
Davit Barbakadze <jayarjo@gmail.com>
2013-03-07 19:43:02 +0400

    Examples: Bring back no-cache headers to upload.php, since without them, iOS devices (and probably some others) are caching it.

--- examples/upload.php
+++ examples/upload.php
@@ -9,6 +9,14 @@
  * Contributing: http://www.plupload.com/contributing
  */
 
+// Make sure file is not cached (as it happens for example on iOS devices)
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+
 // 5 minutes execution time
 @set_time_limit(5 * 60);
 
