4cb9cde47c2ca13dc7e1572e51547029698ddf81
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-03-25 13:36:44 -0400

    Set filename on blob for multipart uploads on Amazon S3

--- src/plupload.js
+++ src/plupload.js
@@ -1246,6 +1246,7 @@ plupload.Uploader = function(settings) {
 						});
 
 						// Add file and send it
+						chunkBlob.name = file.name
 						formData.append(up.settings.file_data_name, chunkBlob);								
 						xhr.send(formData, {
 							runtime_order: up.settings.runtimes,
