60547e49512a4b2b4e4a6ad32b04d85f652addb5
Davit Barbakadze <jayarjo@gmail.com>
2013-06-26 23:34:46 +0400

    Tests: Plupload static methods.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -1,7 +1,7 @@
 {
 	"title": "Plupload", 
 	"tests": [
-		{"title": "plupload", "url": "Plupload.html"},
+		{"title": "plupload", "url": "plupload.html"},
 		{"title": "plupload.Uploader", "url": "Uploader.html"},
 		{"title": "UI Widget", "url": "UI.html"}
 	]
