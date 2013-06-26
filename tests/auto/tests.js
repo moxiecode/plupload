1dedf04e37313304f02e58f9689706f1b57f290c
Davit Barbakadze <jayarjo@gmail.com>
2013-06-26 23:35:16 +0400

    Tests: Uploader initialization.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -2,6 +2,7 @@
 	"title": "Plupload", 
 	"tests": [
 		{"title": "plupload", "url": "plupload.html"},
+		{"title": "plupload.Uploader.init()", "url": "Uploader.init.html"},
 		{"title": "plupload.Uploader", "url": "Uploader.html"},
 		{"title": "UI Widget", "url": "UI.html"}
 	]
