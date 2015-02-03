d8f9687dba6031db7bd594def345f68b22ee7a1c
Steve <info@stevehaar.com>
2015-02-03 12:04:04 -0600

    Update moxie.js
    It appears that the multiple file upload bug only affects v7.0 - 7.0.3

--- js/moxie.js
+++ js/moxie.js
@@ -6120,7 +6120,7 @@ define("moxie/runtime/html5/Runtime", [
 					// it is buggy on Safari Windows and iOS
 					return I.can('select_file') && 
 						!(Env.browser === 'Safari' && Env.os === 'Windows') && 
-						!(Env.os === 'iOS' && Env.verComp(Env.osVersion, "7.0.4", '<'));
+						!(Env.os === 'iOS' && Env.verComp(Env.osVersion, "7.0.0", '>=') && Env.verComp(Env.osVersion, "7.0.4", '<'));
 				},
 				send_binary_string: Test(window.XMLHttpRequest && (new XMLHttpRequest().sendAsBinary || (window.Uint8Array && window.ArrayBuffer))),
 				send_custom_headers: Test(window.XMLHttpRequest),
