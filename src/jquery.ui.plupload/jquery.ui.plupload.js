b79651c34855c14324c11c6fb2538a7ed060b0b9
Davit Barbakadze <jayarjo@gmail.com>
2015-07-03 16:45:47 +0400

    UI: Store the options after they are merged and normalised. Close #1264.

--- src/jquery.ui.plupload/jquery.ui.plupload.js
+++ src/jquery.ui.plupload/jquery.ui.plupload.js
@@ -416,6 +416,9 @@ $.widget("ui.plupload", {
 
 		uploader = this.uploader = uploaders[id] = new plupload.Uploader($.extend(this.options, options));
 
+		// retrieve full normalized set of options
+		this.options = uploader.getOption();
+
 		if (self.options.views.thumbs) {
 			uploader.settings.required_features.display_media = true;
 		}
