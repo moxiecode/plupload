49e610c41ff4194ba12909ebb5a32c4fa20e29cb
Davit Barbakadze <jayarjo@gmail.com>
2016-08-24 07:56:46 +0400

    ImageResizer: Add.

--- src/ImageResizer.js
+++ src/ImageResizer.js
@@ -14,8 +14,9 @@
 */
 define("plupload/ImageResizer", [
 	'plupload',
+	'plupload/core/Queueable',
 	'moxie/image/Image'
-], function(plupload, Image) {
+], function(plupload, Queueable, mxiImage) {
 
 	/**
 	Image preloading and manipulation utility. Additionally it provides access to image meta info (Exif, GPS) and raw binary data.
@@ -23,6 +24,64 @@ define("plupload/ImageResizer", [
 	@class plupload/Image
 	@constructor
 	*/
-	plupload.Image = Image;
+	function ImageResizer(fileRef, options) {
 
+		Queueable.call(this);
+
+		this._options = {
+			width: 0,
+			height: 0,
+			type: 'image/jpeg',
+			quality: 90,
+			crop: false,
+			fit: true,
+			preserveHeaders: true,
+			resample: 'default',
+			multipass: true
+		};
+
+		this.setOption = function(option, value) {
+			if (typeof(option) !== 'object' && !this._options.hasOwnProperty(option)) {
+				return;
+			}
+			ImageResizer.prototype.setOption.call(this);
+		};
+
+
+		this.start = function(options) {
+			var self = this;
+			var options = self.getOptions();
+			var img;
+
+			this.setOptions(options.resize);
+
+			img = new mxiImage();
+
+			img.bind('load', function() {
+				this.resize(options);
+			});
+
+			img.bind('resize', function() {
+				self.done(this.getAsBlob(options.type, options.quality));
+				this.destroy();
+			});
+
+			img.bind('error', function() {
+				self.failed();
+				this.destroy();
+			});
+
+			img.load(file);
+		};
+
+		this.setOptions(option.resize);
+	}
+
+
+	File.prototype = new Queueable();
+
+
+	return ImageResizer;
 });
+
+
