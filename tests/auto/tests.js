03d63962b92e980763b4e1abbbaa5ab4305d659e
Davit Barbakadze <jayarjo@gmail.com>
2016-08-24 08:10:03 +0400

    Tests, Options: Separate options related tests into a separate suite.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -4,6 +4,7 @@
 		{"title": "plupload", "url": "plupload.html"},
 		{"title": "plupload.Uploader.init()", "url": "Uploader.init.html"},
 		{"title": "plupload.Uploader", "url": "Uploader.html"},
+		{"title": "Options", "url": "options.html"},
 		{"title": "Filters", "url": "filters.html"},
 		{"title": "Events", "url": "events.html"},
 		{"title": "Image Resizing", "url": "Resizing.html"},
