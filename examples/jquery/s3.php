0070f0a34df5e6b31048a5697cef8cbecdd4f3b9
Spocke <spocke@moxiecode.com>
2021-03-29 10:18:41 +0200

    disabled s3.php as well

--- examples/jquery/s3.php
+++ examples/jquery/s3.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+#!! IMPORTANT: 
+#!! this file is just an example, it doesn't incorporate any security checks and 
+#!! is not recommended to be used in production environment as it is. Be sure to 
+#!! revise it and customize to your needs.
+die("Make sure that you enable some form of authentication before removing this line.");
+
 /* 
 In order to upload files to S3 using Flash runtime, one should start by placing crossdomain.xml into the bucket.
 crossdomain.xml can be as simple as this:
